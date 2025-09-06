d11437fd6b6e27d31a4999b55d82790d8e91c7a6
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-06 21:26:57 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,36 @@
+
+#Data types elements--->Array ke har element ka ek type hota hai (integer, float, string, boolean, etc).
+
+
+
+
+ #Example: Integers
+
+
+import numpy as np
+arr_integers = np.array([1,3,5,8])
+print("dtype:",arr_integers.dtype)
+
+
+#Example : float
+import numpy as np
+arr_integers = np.array([1,3.5,5,8])
+print("dtype:",arr_integers.dtype)
+print("Array:",arr_integers)
+
+
+#Example:string
+
+import numpy as np
+arr_integers = np.array(["apple","banana","mango"])
+print("dtype:",arr_integers.dtype)
+print("Array:",arr_integers)
+
+
+##Example:boolean
+import numpy as np
+arr_integers = np.array([True, False,True])
+print("dtype:",arr_integers.dtype)
+print("Array:",arr_integers)
+
+
