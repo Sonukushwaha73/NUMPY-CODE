ed15625c84b2456e6592016b7135804743f9d65f
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-06 19:50:07 +0530

    Add files via upload

---
+++ creating identity matrix.py
@@ -0,0 +1,72 @@
+
+#create the identity matrix
+#eye (size apne aap fixed ho jata hai)
+
+
+import numpy as np
+
+identity_eye = np.eye(3)
+print(identity_eye)
+
+
+import numpy as np
+
+identity_eye = np.eye(4,8)
+print(identity_eye)
+
+
+
+
+import numpy as np
+
+#identity_eye = (np.identity(4, k = 1))#
+#print(identity_eye)
+print(np.eye(3, k=-1))   # diagonal -1 position pe
+   
+
+
+
+
+
+#eye wala hai
+
+import numpy as np
+identity_eye =( np.eye(3))
+print(np.eye)
+
+
+
+import numpy as np
+print(np.diag([[1,2,3],
+              [5,6,9]]))
+
+
+
+
+
+#np.identity()
+#Ye n x n identity matrix banata hai.
+
+#Ye n x n identity matrix banata hai.
+
+# Identity matrix ka matlab: diagonal me 1, baaki sab 0.
+
+# Isme shift (k) ka option nahi hota.
+
+
+
+import numpy as np
+#matrix_identity = (np.identity(3))
+print(np.identity(3))
+
+
+
+
+import numpy as np
+
+print(np.diag([[1,4,9],7,9,4]))
+print()
+
+
+
+
