8faac5930f59a44979244d94a95bf9ec40e421cd
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-06 21:27:22 +0530

    Add files via upload

---
+++ ndim array.py
@@ -0,0 +1,28 @@
+
+#ndim array------>Array ka kitne dimensions hain (1D, 2D, 3D, ...).
+ 
+#1D array
+
+import numpy as np
+arr1 = np.array([1,2,3,4,])
+print("array:",arr1)
+print("ndim:", arr1.ndim)
+
+
+
+#2D arrayimport numpy as np
+arr2 = np.array([[1,2,3,4,],[1,7,9,8]])
+print("array:",arr2)
+print("ndim:", arr2.ndim)
+
+
+
+#3D array
+
+arr3 = np.array([[1,2,3,4,],[1,7,9,8],[7,9,3,2]])
+print("array:",arr3)
+print("ndim:", arr3.ndim)  
+
+
+
+
