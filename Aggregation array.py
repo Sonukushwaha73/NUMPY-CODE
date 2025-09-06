98cc710836522bdb15abc4abc06aac0b81d5f9a0
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-06 22:31:12 +0530

    Aggregation array.py

---
+++ Aggregation array.py
@@ -0,0 +1,27 @@
+
+#Aggregation----- ke mtlb kishi element ko jodkar ek result niklna hota hai
+
+
+# np.sum() → sabko add karna
+
+# np.mean() → average niklna
+
+# np.min() → small  element
+
+# np.max() → big element
+
+# np.std() → standard deviation (spread check karne ke liye
+
+
+import numpy as np
+arr = np.array([1,3,4,6])
+print("sum:",np.sum(arr))
+print("average:",np.average(arr))
+
+print("mean:",np.mean(arr))
+print("min:",np.min(arr))
+print("max:",np.max(arr))
+print("standard deviation:",np.std(arr))
+
+
+
