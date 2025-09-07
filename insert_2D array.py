588e6e82508e08d9192251f29f250fe5d72fd743
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-07 16:20:59 +0530

    Add files via upload

---
+++ insert_2D array.py
@@ -0,0 +1,23 @@
+# row 0
+# column 1
+# none pura ek me kar dega
+
+import numpy as np
+arr = np.array([[10,20,30,45],[89,70,65,78]])
+arr_2D= np.insert(arr,2,[5,6],axis =1)# [[10 20  5 30 45]
+
+print(arr_2D)
+print(arr)
+
+
+
+import numpy as np
+arr = np.array([[10,20,30,45],[89,70,65,78]])
+arr_2D= np.insert(arr,2,[5,6],axis = none)# [[10 20  5 30 45]
+
+print(arr_2D)
+print(arr)
+
+
+
+
