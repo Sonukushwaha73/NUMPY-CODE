51c1f363cce354624e89acc36e6c504ddf83fd6b
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-07 16:20:04 +0530

    RESHAPING AND MANUPULTING

---
+++ flattening array .py
@@ -0,0 +1,13 @@
+#revel()----views originals  value is modification
+
+#fitten---> copy is return originals  value nhi rahega
+
+
+# Flatten = new ghar banana hota hai 🏠
+# 👉 Ravel = usi ghar ke room shidha karna hota hai 🛋️
+
+import numpy as np
+
+arr = np.array([[12,3,67,98],[2,45,67,32]])
+print(arr.ravel()) #sab ke sath ho jayege
+print(arr.flatten())
